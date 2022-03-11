a1c402a0b78c9f5d80e92d0ca527d9797c5ae500
anthony sanchez <anthonysa0813@gmail.com>
2022-03-11 18:33:32 -0500

    palindromo info

---
+++ algoritmia/04-ejercicio.js
@@ -0,0 +1,15 @@
+function marioBuild(height) {
+  for (let i = 0; i <= height; i++) {
+    console.log(`${"#" * i} \n`);
+  }
+}
+
+marioBuild(3);
+
+/* 
+  #
+ ##
+###
+
+
+*/
---
+++ algoritmia/06-polindromos.js
@@ -0,0 +1,11 @@
+/* 
+  Existen palabras que al derecho y alrevés la palabra se lee igual
+  por ejemplo: ana, ada
+  Realiza una función que identifique si es o no un palindrono.
+
+
+*/
+
+function palindromoWord(word) {}
+
+palindromoWord("ana");
